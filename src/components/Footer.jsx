b93199a5ffe9a35211b2212f5d99b12061235db9
Ramachandran <ramachandra.ramu001@gmail.com>
2026-05-19 14:40:54 +0530

    Remove Instagram import from Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { Mail, Phone, MapPin, Facebook, Twitter, Linkedin, Instagram } from "lucide-react";
+import { Mail, Phone, MapPin, Facebook, Twitter, Linkedin, } from "lucide-react";
 
 const Footer = () => {
   return (
