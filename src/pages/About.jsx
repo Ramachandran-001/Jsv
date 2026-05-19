46e73f32cd751a8cbe568c4244e4406d01e4958d
Ramachandran <ramachandra.ramu001@gmail.com>
2026-05-19 14:10:19 +0530

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -20,7 +20,7 @@ const aboutData = [
   {
     title: "Solar Energy",
     description: "Awarded 'Green Tech Innovator of the Year' and celebrated for setting new industry benchmarks in efficiency.",
-    image: "/public/image/solar.png",
+    image: "/image/solar.png",
   },
 ];
 
